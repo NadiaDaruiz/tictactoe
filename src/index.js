81abff846a4bd1398423e70d5e16a6ba5a75a7b5
Nadia Daruiz <nadiadaruiz@gmail.com>
2020-11-17 11:17:31 +0100

    ready to be push into github

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Confetti from 'react-confetti';
+import './style.css';
 
 
-const Square = (props) => {
+function Square(props) {
   return (
     <button
       className="square"
-      onClick={() => props.onClick}>
+      onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -15,47 +16,19 @@ const Square = (props) => {
 
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
 
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
-      <div>
-        <div className="status">{status}</div>
+      <div className="outer-border">
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -77,15 +50,75 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li className="moves-list" key={move}>
+          <button className="btn-moves" onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    });
+
+    let status;
+    if (winner) {
+      status = '...and the WINNER is  ' + winner + '!';
+
+    } else {
+      status = 'next player is ' + (this.state.xIsNext ? 'X' : '0');
+    }
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+      <div className="game-container">
+        { winner && <Confetti />}
+        <h2 className="title">TIC TAC TOE</h2>
+        <div className="game-status">{status}</div>
+        <div className="game-box">
+          <div className="game-subcontainer">
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+            <ol className="each-move">{moves}</ol>
+          </div>
         </div>
       </div>
     );
@@ -118,4 +151,5 @@ function calculateWinner(squares) {
     }
   }
   return null;
+
 }
